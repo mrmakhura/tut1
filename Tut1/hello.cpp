9ea8faf655ca16ccf613b58385c5e5cef9abe59a
John Poole <poolej@ukzn.ac.za>
2015-02-25 12:30:29 +0200

    add personalized greeting

--- Tut1/hello.cpp
+++ Tut1/hello.cpp
@@ -1,7 +1,12 @@
 // John Poole, CM3, 25/02/2015
 // My first C++ program (Hello, World!)
 #include <iostream>
+#include <string>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	std::string myName;
+
+	std::cout << "Please enter your name: ";
+	std::cin >> myName;
+	std::cout << "Hello, " + myName << std::endl;
 }
